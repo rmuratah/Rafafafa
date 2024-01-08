ac6ae3b707922cb1e9a11262840b01da5fd78fd4
rmuratah <rahmurata12@gmail.com>
2024-01-08 16:22:46 -0300

    First Commit

---
+++ src/presentation/components/header/header.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const header = () => {
+
+    return (
+        <Header>
+            dasdasd
+        </Header>
+    )
+}
+
+export default header;
+
+const Header = styled.header`
+    background-color: red;
+`
